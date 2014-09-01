6e1bbc6e2c741d9843889391a7ae85439636345d
Chiara Paci <chiara.paci@gmail.com>
2014-09-01 13:47:11 +0200

    Added Libre Baskerville

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_third',
-    version='0.3',
+    version='0.4',
     packages=['santaclara_third'],
     package_data={'santaclara_third': [
             'static/css/*.css',
