a5e5933151d3f9feedde54bff6ea6d3e9c6b717c
Chiara Paci <chiara.paci@gmail.com>
2015-01-15 08:54:12 +0100

    Added creativecommons.css 1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='santaclara_third',
-    version='0.8.2',
+    version='0.9.1',
     packages=['santaclara_third'],
     package_data={'santaclara_third': [
             'static/css/*.css',
